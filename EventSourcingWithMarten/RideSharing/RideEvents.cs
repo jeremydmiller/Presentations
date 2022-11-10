6f1ce8bcdd41b44fe0973d5005d1dbbf12befbbd
Jeremy D. Miller <jeremydmiller@yahoo.com>
2022-11-10 05:46:33 -0600

    updates to sample code

--- EventSourcingWithMarten/RideSharing/RideEvents.cs
+++ EventSourcingWithMarten/RideSharing/RideEvents.cs
@@ -5,7 +5,46 @@ public class Customer
     public Guid Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public 
 }
 
-public record RideRequested();
+public class Driver
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public record ShiftStarted(Guid DriverId);
+public record DriverReady(Location Location);
+public record RideAccepted(Guid RideId);
+
+public record DriverLocated(Location location);
+
+
+public class DriverShift
+{
+    public Guid Id { get; set; }
+
+    public DriverShift(ShiftStarted started)
+    {
+        DriverId = started.DriverId;
+    }
+
+    public Guid DriverId { get; set; }
+}
+
+public record Location(double Latitude, double Longitude);
+
+public record RideRequested(Guid CustomerId, Location Location);
+public record DriverAssigned(Guid DriverId, string Category);
+
+public record RidePriced(decimal Amount, string Category);
+
+public record DriverArrived();
+
+public record RideStarted();
+public record RideEnded();
+
+
+public record RideCancelled();
+
